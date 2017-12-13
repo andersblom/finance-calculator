734a8e33aa448d50b32e8f7c7044088573338bd1
Anders Blom <anders@andersblom.dk>
2017-12-13 07:33:09 -0500

    Initialized React Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
@@ -7,5 +8,8 @@ import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
