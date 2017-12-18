801d3ba507d18d36556689a25c67498ca9882813
Anders Blom <anders@andersblom.dk>
2017-12-18 08:58:03 -0500

    Added logic for clearing AddBudgetItem forms on submit

--- src/components/WorkSheet/BudgetItems/index.js
+++ src/components/WorkSheet/BudgetItems/index.js
@@ -21,7 +21,14 @@ export default class BudgetItems extends Component {
     const isExpense = e.target.isExpense !== undefined;
 
     this.props.addBudgetItem(name, amount, isExpense);
+
+    if (isExpense === true) {
+      this.expenseForm.reset();
+    } else {
+      this.incomeForm.reset();
+    }
   }
+
   render() {
     const income = this.props.items.filter(item => item.expense === false);
     const expenses = this.props.items.filter(item => item.expense === true);
@@ -31,7 +38,7 @@ export default class BudgetItems extends Component {
         <div className="budgetItems__category">
           <h2>Income</h2>
           <div>{renderListOfItems(income)}</div>
-          <form onSubmit={this.handleSubmitNewBudgetItem}>
+          <form ref={(el) => { this.incomeForm = el; }} onSubmit={this.handleSubmitNewBudgetItem}>
             <input type="text" className="budgetItems__addNew name" name="name" placeholder="new income item" />
             <input type="number" className="budgetItems__addNew amount" name="amount" placeholder="amount" />
             <input type="submit" className="budgetItems__addNew submit" />
@@ -40,7 +47,7 @@ export default class BudgetItems extends Component {
         <div className="budgetItems__category">
           <h2>Expenses</h2>
           <div>{renderListOfItems(expenses)}</div>
-          <form onSubmit={this.handleSubmitNewBudgetItem}>
+          <form ref={(el) => { this.expenseForm = el; }} onSubmit={this.handleSubmitNewBudgetItem}>
             <input type="text" className="budgetItems__addNew name" name="name" placeholder="new income item" />
             <input type="number" className="budgetItems__addNew amount" name="amount" placeholder="amount" />
             <input type="submit" className="budgetItems__addNew submit" />
