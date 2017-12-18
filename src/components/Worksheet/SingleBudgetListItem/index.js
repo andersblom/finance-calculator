cd73f664d3443b18e38aab068417d2d0427769f9
Anders Blom <anders@andersblom.dk>
2017-12-18 13:42:28 -0500

    Refactoring SingleBudgetListItem into ES6 class Laid out some groundwork for toggling editing on single budget list-items. They need individual state to determine if it's being edited or not + a bunch of class-functions that will handle clicks / call editItem reducer and UX details like focus()/blur()

--- src/components/Worksheet/SingleBudgetListItem/index.js
+++ src/components/Worksheet/SingleBudgetListItem/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './SingleBudgetListItem.css';
 
-const SingleBudgetListItem = ({ item }) => (
-  <div className="listItem__entry__container">
-    <div className="name">{item.name}</div>
-    <div className="amount">{item.amount}</div>
-    <div className="actions"><button>Edit</button><button>Delete</button></div>
-  </div>
-);
+class SingleBudgetListItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+    };
+
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+  handleEditClick() {
+    this.setState({
+      editing: true,
+    }, () => {
+      const temporaryValue = this.editInputName.value;
+      this.editInputName.focus();
+      this.editInputName.value = '';
+      this.editInputName.value = temporaryValue;
+    });
+  }
+  render() {
+    const { item, editBudgetItem } = this.props;
+    const { editing } = this.state;
+    return (
+      editing
+        ?
+          <div className="listItem__entry__container">
+            <div className="name"><input type="text" value={item.name} ref={(el) => { this.editInputName = el; }} /></div>
+            <div className="amount">{item.amount}</div>
+            <div className="actions"><button onClick={this.handleEditClick}>Edit</button><button>Delete</button></div>
+          </div>
+        :
+          <div className="listItem__entry__container">
+            <div className="name">{item.name}</div>
+            <div className="amount">{item.amount}</div>
+            <div className="actions"><button onClick={this.handleEditClick}>Edit</button><button>Delete</button></div>
+          </div>
+    );
+  }
+}
 
 SingleBudgetListItem.propTypes = {
   item: PropTypes.shape({
