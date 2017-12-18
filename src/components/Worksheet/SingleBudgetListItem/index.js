c0bf47c3e56270b2443028419ae6943976e42b8b
Anders Blom <anders@andersblom.dk>
2017-12-18 14:34:30 -0500

    SingleBudgetListItem handling of user actions and data handoff to reducer

--- src/components/Worksheet/SingleBudgetListItem/index.js
+++ src/components/Worksheet/SingleBudgetListItem/index.js
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types';
 import './SingleBudgetListItem.css';
 
 class SingleBudgetListItem extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       editing: false,
+      name: props.item.name,
+      amount: props.item.amount,
     };
 
     this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
   }
+
   handleEditClick() {
     this.setState({
       editing: true,
@@ -22,17 +28,38 @@ class SingleBudgetListItem extends Component {
       this.editInputName.value = temporaryValue;
     });
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.editBudgetItem(this.props.item.id, this.state.name, this.state.amount);
+    this.setState({
+      editing: false,
+    });
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handleAmountChange(e) {
+    this.setState({
+      amount: e.target.value,
+    });
+  }
+
   render() {
-    const { item, editBudgetItem } = this.props;
+    const { item } = this.props;
     const { editing } = this.state;
     return (
       editing
         ?
-          <div className="listItem__entry__container">
-            <div className="name"><input type="text" value={item.name} ref={(el) => { this.editInputName = el; }} /></div>
-            <div className="amount">{item.amount}</div>
-            <div className="actions"><button onClick={this.handleEditClick}>Edit</button><button>Delete</button></div>
-          </div>
+          <form onSubmit={this.handleSubmit} className="listItem__entry__container">
+            <div className="name"><input type="text" value={this.state.name} onChange={this.handleNameChange} ref={(el) => { this.editInputName = el; }} /></div>
+            <div className="amount"><input type="number" value={this.state.amount} onChange={this.handleAmountChange} /></div>
+            <div className="actions"><input type="submit" onClick={this.handleSubmit} value="Done" /></div>
+          </form>
         :
           <div className="listItem__entry__container">
             <div className="name">{item.name}</div>
@@ -45,10 +72,12 @@ class SingleBudgetListItem extends Component {
 
 SingleBudgetListItem.propTypes = {
   item: PropTypes.shape({
-    name: PropTypes.string,
-    amount: PropTypes.number,
-    expense: PropTypes.bool,
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired,
+    expense: PropTypes.bool.isRequired,
   }).isRequired,
+  editBudgetItem: PropTypes.func.isRequired,
 };
 
 export default SingleBudgetListItem;
