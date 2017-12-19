e25eeeb5262100d36dac340747bbb9976fb439c7
Anders Blom <anders@andersblom.dk>
2017-12-19 15:03:49 -0500

    installed nodemon to activate start:server script and excluded console log eslint rule for /api

--- api/start.js
+++ api/start.js
@@ -3,8 +3,9 @@ require('dotenv').config({
 });
 
 const app = require('./app');
+
 const port = process.env.API_PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`API running on port ${port}`);
+	console.log(`API running on port ${port}`);
 });
