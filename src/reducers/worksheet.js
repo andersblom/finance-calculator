22e66637e3d917f200a74637e11f31a357773a02
Anders Blom <anders@andersblom.dk>
2017-12-19 08:06:01 -0500

    set initial state to nothing, to handle fresh worksheets

--- src/reducers/worksheet.js
+++ src/reducers/worksheet.js
@@ -2,39 +2,8 @@ import uuidv4 from 'uuid/v4';
 import { CHANGE_WORKSHEET_TITLE, ADD_BUDGET_ITEM, EDIT_BUDGET_ITEM, DELETE_BUDGET_ITEM } from '../actions/actionTypes';
 
 const initialState = {
-  title: 'My awesome worksheet',
-  items: [
-    {
-      name: 'Monthly salary',
-      amount: 4000,
-      expense: false,
-      id: uuidv4(),
-    },
-    {
-      name: 'Apartment rent',
-      amount: 1545,
-      expense: true,
-      id: uuidv4(),
-    },
-    {
-      name: 'Food',
-      amount: 800,
-      expense: true,
-      id: uuidv4(),
-    },
-    {
-      name: 'Electric/Water/Internet',
-      amount: 300,
-      expense: true,
-      id: uuidv4(),
-    },
-    {
-      name: 'Fun times',
-      amount: 500,
-      expense: true,
-      id: uuidv4(),
-    },
-  ],
+  title: null,
+  items: [],
 };
 
 export const worksheet = (state = initialState, action) => {
