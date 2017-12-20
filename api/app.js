b34e1be19d55a2c2f529c12a4a25fabf4c8cd10a
Anders Blom <anders@andersblom.dk>
2017-12-20 09:56:32 -0500

    added morgan + bodyparser middleware

--- api/app.js
+++ api/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(morgan('combined'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
 
 app.use('/', routes);
 
