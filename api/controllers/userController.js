bc216391ade74bc172d18c6f66cc1d0f2d1c54e2
Anders Blom <anders@andersblom.dk>
2017-12-20 11:56:49 -0500

    Wrote some proper error throwing in the userController validateuserinput

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,16 +1,23 @@
 const validator = require('validator');
 
 exports.validateUserInput = (req, res, next) => {
+	if (!req.body.email || !req.body.fullName || !req.body.password) {
+		const error = new Error('One or more fields were left out');
+		error.status = 400;
+		return next(error);
+	}
 	if (!validator.isEmail(req.body.email)) {
 		res.status(400);
-		res.json({
-			message: 'error',
-		});
+		return next(new Error('Email is not valid.'));
 	}
+	
+	//Check if user already exists
+	// ---
+
 	next();
 };
 
-exports.createNewUser = (req, res) => {
+exports.createNewUser = (req, res, next) => {
 	res.status(200);
 	res.send(req.body);
 };
