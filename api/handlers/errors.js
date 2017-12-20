bc1f408dfc74b6e91cf351e9c0e2a024855736bd
Anders Blom <anders@andersblom.dk>
2017-12-20 11:56:22 -0500

    some optimizing in the error handler

--- api/handlers/errors.js
+++ api/handlers/errors.js
@@ -5,9 +5,9 @@ exports.logErrors = (err, req, res, next) => {
 
 exports.sendClientErrors = (err, req, res, next) => {
 	if (req.xhr) {
-		res.status(500);
+		res.status(err.status || 500);
 		res.json({
-			message: 'Something went wrong!',
+			message: err.message || 'Something went wrong!',
 		});
 	} else {
 		next(err);
@@ -15,9 +15,10 @@ exports.sendClientErrors = (err, req, res, next) => {
 };
 
 exports.catchAllErrors = (err, req, res, next) => {
-	res.status(500);
+	res.status(err.status || 500);
+	console.log(err);
 	res.json({
-		message: 'Something went wrong!',
+		message: err.message || 'Something went wrong!',
 		error: err
 	});
 };
