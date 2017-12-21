bb401e082676fd1932d7523bcefd447881dec2e8
Anders Blom <anders@andersblom.dk>
2017-12-21 08:13:37 -0500

    No need for the friendly api root message ;)

--- api/__tests__/routes.test.js
+++ api/__tests__/routes.test.js
@@ -27,7 +27,7 @@ describe('Test API status', () => {
 	test('It should be online, responding to a GET /', () => {
 		return request(app).get('/').then(res => {
 			// Checking for statusCode to be 200 (OK)
-			expect(res.statusCode).toBe(200);
+			expect(res.statusCode).toBe(403);
 			// Checking to make sure there is a "message" key on the response object
 			expect(Object.keys(res.body)).toEqual(['message']);
 		});
--- api/controllers/apiController.js
+++ api/controllers/apiController.js
@@ -1,6 +1,6 @@
 exports.checkApiStatus = (req, res) => {
-	res.status(200);
+	res.status(403);
 	res.json({
-		message: 'Welcome to the API. This is the root route, and if you are seeing this, that means the API is up and running.'
+		message: 'Nothing to see here.'
 	});
 };
