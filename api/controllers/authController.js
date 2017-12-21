b68d94eb3832a5be7a81edbbdf0ca0547bae9c6d
Anders Blom <anders@andersblom.dk>
2017-12-21 09:37:10 -0500

    login-route Checking user email + password against db

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,36 @@
-exports.login = async (req, res) => {
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+require('../models/User');
+
+const User = mongoose.model('User');
+
+exports.login = async (req, res, next) => {
+	const findUser = await User.findOne({ email: req.body.email });
+
+	if (findUser === null) {
+		const error = new Error('Email and password does not match.');
+		error.status = 401; 
+		return next(error);
+	}
+
+	const userPwIsCorrect = await bcrypt.compare(req.body.password, findUser.password)
+		.then(res => {
+			return res;
+		})
+	;
 	
+	if (!userPwIsCorrect) {
+		const error = new Error('Email and password does not match.');
+		error.status = 401; 
+		return next(error);
+	}
+
+	res.status(200);
+	res.json({
+		message: 'Success! You are logged in.'
+	});
+
 	// Send JWT 
 
 };
