6dfc349bf5332ed5fe1cff2bc9173fcea808d8b5
Anders Blom <anders@andersblom.dk>
2018-01-18 21:11:26 -0600

    refactored Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,45 +5,36 @@ import { lifecycle } from 'recompose';
 
 import './Header.css';
 
-type Props = {
-  isLoggedIn: boolean
-}
-
-const Header = (props: Props) => {
-  const { isLoggedIn } = props;
-  return (
-    <header>
-      <div className="app__logo">
-        <span className="emoji__logo" role="img" aria-label="Money with wings">💸</span>
-        <div className="app__logo__text">...</div>
-      </div>
-      <nav className="appNav">
-        {
-          isLoggedIn
-          ?
-            <ul>
-              <li><Link to="/docs">Docs</Link></li>
-              <li><Link to="/user/account">Account</Link></li>
-              <li><Link to="/user/logout">Log out</Link></li>
-            </ul>
-          :
-            <ul>
-              <li><Link to="/docs">Docs</Link></li>
-              <li><Link to="/login">Log in</Link></li>
-              <li><Link to="/signup">Sign up</Link></li>
-            </ul>
-        }
-      </nav>
-    </header>
-  );
-};
-
-Header.defaultProps = {
-  isLoggedIn: false, // Should be false after css/markup is done
-};
-
-export default lifecycle({
-  componentDidMount() {
-    console.log("Mounted Header");
+const Header = (
+  {
+    isLoggedIn
+  }:{
+    isLoggedIn: Boolean
   }
-})(Header);
+) => (
+  <header>
+    <div className="app__logo">
+      <span className="emoji__logo" role="img" aria-label="Money with wings">💸</span>
+      <div className="app__logo__text">...</div>
+    </div>
+    <nav className="appNav">
+      {
+        isLoggedIn
+        ?
+          <ul>
+            <li><Link to="/docs">Docs</Link></li>
+            <li><Link to="/user/account">Account</Link></li>
+            <li><Link to="/user/logout">Log out</Link></li>
+          </ul>
+        :
+          <ul>
+            <li><Link to="/docs">Docs</Link></li>
+            <li><Link to="/login">Log in</Link></li>
+            <li><Link to="/signup">Sign up</Link></li>
+          </ul>
+      }
+    </nav>
+  </header>
+);
+
+export default Header;
