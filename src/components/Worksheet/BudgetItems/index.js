55d24fd1a20d7804c6096f32a768a935a0ea915d
Anders Blom <anders@andersblom.dk>
2018-01-19 20:57:00 -0600

    refactored BudgetItems

--- src/components/Worksheet/BudgetItems/index.js
+++ src/components/Worksheet/BudgetItems/index.js
@@ -5,74 +5,78 @@ import SingleBudgetListItem from '../SingleBudgetListItem';
 
 import './BudgetItems.css';
 
-export default class BudgetItems extends Component {
-  constructor() {
-    super();
-    this.handleSubmitNewBudgetItem = this.handleSubmitNewBudgetItem.bind(this);
-  }
+const handleSubmitNewBudgetItem = (e) => {
+  e.preventDefault();
+  const name = e.target.name.value.length === 0 ? 'Something mc somethingson' : e.target.name.value;
+  const amount = Number.isNaN(parseInt(e.target.amount.value, 10))
+    ? 0
+    : parseInt(e.target.amount.value, 10);
+  const isExpense = e.target.isExpense !== undefined;
 
-  handleSubmitNewBudgetItem(e) {
-    e.preventDefault();
-    const name = e.target.name.value.length === 0 ? 'Something mc somethingson' : e.target.name.value;
-    const amount = Number.isNaN(parseInt(e.target.amount.value, 10)) ? 0 : parseInt(e.target.amount.value, 10);
-    const isExpense = e.target.isExpense !== undefined;
+  this.props.addBudgetItem(name, amount, isExpense);
 
-    this.props.addBudgetItem(name, amount, isExpense);
-
-    if (isExpense === true) {
-      this.expenseForm.reset();
-      this.expenseForm.name.focus();
-    } else {
-      this.incomeForm.reset();
-      this.incomeForm.name.focus();
-    }
-  }
-
-  renderListOfItems(items) {
-    if (items.length !== 0) {
-      return items.map(item => <SingleBudgetListItem deleteBudgetItem={this.props.deleteBudgetItem} editBudgetItem={this.props.editBudgetItem} key={item.id} item={item} />);
-    }
-
-    return <div className="budgetitems__noFieldAdded">Ow. It&apos;s pretty lonely here :(</div>;
-  }
-
-  render() {
-    const income = this.props.items.filter(item => item.expense === false);
-    const expenses = this.props.items.filter(item => item.expense === true);
-
-    return (
-      <div className="budgetItems__container">
-        <div className="budgetItems__category">
-          <h2>Income</h2>
-          <div>{this.renderListOfItems(income)}</div>
-          <form ref={(el) => { this.incomeForm = el; }} onSubmit={this.handleSubmitNewBudgetItem}>
-            <input type="text" className="budgetItems__addNew name" name="name" placeholder="new income item" />
-            <input type="number" className="budgetItems__addNew amount" name="amount" placeholder="amount" />
-            <input type="submit" className="budgetItems__addNew submit" />
-          </form>
-        </div>
-        <div className="budgetItems__category">
-          <h2>Expenses</h2>
-          <div>{this.renderListOfItems(expenses)}</div>
-          <form ref={(el) => { this.expenseForm = el; }} onSubmit={this.handleSubmitNewBudgetItem}>
-            <input type="text" className="budgetItems__addNew name" name="name" placeholder="new income item" />
-            <input type="number" className="budgetItems__addNew amount" name="amount" placeholder="amount" />
-            <input type="submit" className="budgetItems__addNew submit" />
-            <input type="checkbox" className="budgetItems__addNew hiddenCheckbox" name="isExpense" checked hidden readOnly />
-          </form>
-        </div>
-      </div>
-    );
+  if (isExpense === true) {
+    this.expenseForm.reset();
+    this.expenseForm.name.focus();
+  } else {
+    this.incomeForm.reset();
+    this.incomeForm.name.focus();
   }
 }
 
-BudgetItems.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.object),
-  addBudgetItem: PropTypes.func.isRequired,
-  editBudgetItem: PropTypes.func.isRequired,
-  deleteBudgetItem: PropTypes.func.isRequired,
-};
+function renderListOfItems(items) {
+  if (items.length !== 0) {
+    return items.map(item => (
+      <SingleBudgetListItem
+        deleteBudgetItem={this.props.deleteBudgetItem}
+        editBudgetItem={this.props.editBudgetItem}
+        key={item.id}
+        item={item}
+      />
+    ));
+  }
+  return <div className="budgetitems__noFieldAdded">Ow. It&apos;s pretty lonely here :(</div>;
+}
+
+const BudgetItems = (
+  {
+    items,
+  }:{
+    items: Array,
+  },
+) => (
+  <div className="budgetItems__container">
+    <div className="budgetItems__category">
+      <h2>Income</h2>
+      <div>{renderListOfItems(items.filter(item => item.expense === false))}</div>
+      <form ref={(el) => { this.incomeForm = el; }} onSubmit={handleSubmitNewBudgetItem}>
+        <input type="text" className="budgetItems__addNew name" name="name" placeholder="new income item" />
+        <input type="number" className="budgetItems__addNew amount" name="amount" placeholder="amount" />
+        <input type="submit" className="budgetItems__addNew submit" />
+      </form>
+    </div>
+    <div className="budgetItems__category">
+      <h2>Expenses</h2>
+      <div>{renderListOfItems(items.filter(item => item.expense === true))}</div>
+      <form ref={(el) => { this.expenseForm = el; }} onSubmit={handleSubmitNewBudgetItem}>
+        <input type="text" className="budgetItems__addNew name" name="name" placeholder="new income item" />
+        <input type="number" className="budgetItems__addNew amount" name="amount" placeholder="amount" />
+        <input type="submit" className="budgetItems__addNew submit" />
+        <input type="checkbox" className="budgetItems__addNew hiddenCheckbox" name="isExpense" checked hidden readOnly />
+      </form>
+    </div>
+  </div>
+);
+
+// BudgetItems.propTypes = {
+//   items: PropTypes.arrayOf(PropTypes.object),
+//   addBudgetItem: PropTypes.func.isRequired,
+//   editBudgetItem: PropTypes.func.isRequired,
+//   deleteBudgetItem: PropTypes.func.isRequired,
+// };
 
 BudgetItems.defaultProps = {
   items: [],
 };
+
+export default BudgetItems;
--- src/components/Worksheet/DisplayTotal/index.js
+++ src/components/Worksheet/DisplayTotal/index.js
@@ -1,16 +1,18 @@
+// @flow
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './DisplayTotal.css';
 
-const DisplayTotal = ({ total }) => (
+const DisplayTotal = (
+  {
+    total,
+  }:{
+    total: number,
+  },
+) => (
   <div className="totalAmount">
     Total: <span className={total >= 0 ? 'pos' : 'neg'}>{total.toLocaleString()}</span>
   </div>
 );
-
-DisplayTotal.propTypes = {
-  total: PropTypes.number.isRequired,
-};
 
 export default DisplayTotal;
